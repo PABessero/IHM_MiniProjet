5ff7cf7e1841dd03ec2f90005c2c83ce36e68031
Pierre-Alain BESSERO <pierrealain.bessero@etudiant.univ-lr.fr>
2018-05-25 14:39:28 +0200

    test

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -14,10 +14,12 @@ public class Main extends Application {
         primaryStage.setTitle("IHM Mini Projet");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
+
     }
 
 
     public static void main(String[] args) {
+
         launch(args);
     }
 }
--- src/sample/mainController.java
+++ src/sample/mainController.java
@@ -1,6 +1,8 @@
 package sample;
 
+import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 
@@ -10,6 +12,14 @@ public class mainController {
     @FXML public TableColumn colType;
     @FXML public TableColumn colDate;
     @FXML public TableColumn colHeure;
+    @FXML public TableColumn colFin;
     @FXML public TableColumn colDuree;
     @FXML public TableColumn colSalle;
+    @FXML public TableColumn colProf;
+    @FXML public ChoiceBox cbMatiere;
+
+    public mainController(){
+        //cbMatiere = new ChoiceBox(FXCollections.observableArrayList(Matiere.AP.nom, Matiere.BD.nom, Matiere.CDIN.nom, Matiere.SI.nom));
+    }
+
 }
